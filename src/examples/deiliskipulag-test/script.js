6c89bec8b0aa01a60a69dad6211cea85a2b05294
zo-lew <zoe.lewis@students.iaac.net>
2022-03-20 01:27:06 +0000

    aesthetic updates

--- src/examples/deiliskipulag-test/script.js
+++ src/examples/deiliskipulag-test/script.js
@@ -1,8 +1,9 @@
 import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.126.0/build/three.module.js'
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/OrbitControls.js'
-import { TransformControls } from "https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/TransformControls.js";
 import { Rhino3dmLoader } from 'https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/loaders/3DMLoader.js'
 import rhino3dm from 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/rhino3dm.module.js'
+import { TransformControls } from "https://cdn.jsdelivr.net/npm/three@0.126.0/examples/jsm/controls/TransformControls.js";
+
 
 // set up loader for converting the results to threejs
 const loader = new Rhino3dmLoader()
